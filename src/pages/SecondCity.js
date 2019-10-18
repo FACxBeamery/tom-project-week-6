ca577a471e28802c26b02e9333fca5835846b47d
Tom Galligan <tom.galligan@beamery.com>
2019-10-18 10:47:15 +0100

    fix typo

--- src/pages/SecondCity.js
+++ src/pages/SecondCity.js
@@ -17,7 +17,7 @@ const SecondCity = ({
 		<div className={main.slide}>
 			<h1>Stage {stage} page</h1>
 			<p>
-				The temperature of {oldCity} is {temp.toFixed(1)} C. Your total
+				The temperature of {city} is {temp.toFixed(1)} C. Your total
 				score so far is {Number(score).toFixed()}.
 			</p>
 			<p> What's your next destination? Enter it here:</p>
